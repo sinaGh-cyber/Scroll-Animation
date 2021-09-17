b25745b177ea2c277cf0e3518eb0dc4d69bdcc44
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-09-17 07:20:29 +0430

    JS READY final

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const boxes = document.querySelectorAll('.box');
+const hight = (window.innerHeight / 5) * 4;
 
+const checkBoxes = () => {
+  boxes.forEach((box) => {
+    box.getBoundingClientRect().top < hight
+      ? box.classList.add('show')
+      : box.classList.remove('show');
+  });
+};
+
+checkBoxes();
+
+window.addEventListener('scroll', checkBoxes);
